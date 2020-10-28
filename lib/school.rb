01207d2130731e5fd8ec4035ee9feb1a1e7d5973
jyliu17 <jyliu17@gmail.com>
2020-10-27 22:54:00 -0400

    done

--- lib/school.rb
+++ lib/school.rb
@@ -4,28 +4,25 @@ require 'pry'
 
 class School
 
-    attr_accessor :name, :roster
+    attr_reader :name, :roster
 
     def initialize(name)
         @name = name
         @roster = {}
     end
-    
-    def add_student(name,grade)
-        roster[grade] ||= []
-        roster[grade] << name
+
+    def add_student(name, grade)
+        self.roster[grade] ||= []
+        self.roster[grade] << name
     end
 
     def grade(students)
-        roster[students]
+        self.roster[students]
     end
 
     def sort
-        school_sort = {}
-        roster.each do |grade, student|
-        school_sort[grade] = student.sort
-        end 
-        school_sort
+        self.roster.each {|grade,students| students.sort!}
     end
+
 
 end
